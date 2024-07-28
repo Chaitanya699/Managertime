0bf2c958f718f92910bc6565a646f375ef6758c8
Chaitanya Khairnar <104911969+Chaitanya699@users.noreply.github.com>
2024-07-28 13:33:57 +0530

    Update3 mongodb.js

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI || 'your_default_local_mongo_uri_here';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://chaitanyakhairnar143:f2ucN2oT7eG2kJyt@chaitu.tz09ufj.mongodb.net/?retryWrites=true&w=majority&appName=chaitu';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let db = null;
